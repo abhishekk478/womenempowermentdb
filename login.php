1348eaeeea66470ccdfd6e0e61bd297d6aad70ff
abhishekk478 <35848340+abhishekk478@users.noreply.github.com>
2018-06-10 17:01:54 +0530

    WomenEmpowerment

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+ 
+// check for post data
+if (isset($_GET["email"]) && isset($_GET["pass"])) {
+    $email = $_GET['email'];
+    $pass = $_GET['pass'];
+ 
+    // get a product from products table
+    $result = mysql_query("SELECT *FROM register WHERE email = $email AND pass = $pass");
+ 
+    if (!empty($result)) {
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+ 
+            $result = mysql_fetch_array($result);
+ 
+            $user = array();
+            $user["email"] = $result["email"];
+            $user["name"] = $result["name"];
+            
+            // success
+            $response["success"] = 1;
+ 
+            // user node
+            $response["user"] = array();
+ 
+            array_push($response["user"], $user);
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no product found
+            $response["success"] = 0;
+            $response["message"] = "No user found";
+ 
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no product found
+        $response["success"] = 0;
+        $response["message"] = "No user found";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
